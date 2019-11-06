abd1e59bed1cbb7dc98768d39b6ca92c41b30077
James Baker <jbaker1@mit.edu>
2019-11-06 04:09:01 +0100

    first commit

---
+++ js/typing.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typed-me", {
+  strings: ["Trucks Venture Capital funds entrepreneurs building the future of mobility.",
+            "Trucks Venture Capital funds entrepreneurs building the future of connected vehicles.",
+            "Trucks Venture Capital funds entrepreneurs building the future of <strong>transportation.</strong>"],
+  typeSpeed: 30,
+  backSpeed: 20,
+  smartBackspace: true, // this is a default
+  startDelay: 2000,
+  showCursor: false
+});
